9ed62f11d397fc37f93527daac5c979fac1cda2c
Mike Mullen <mmullen227@gmail.com>
2019-04-07 16:25:17 -0400

    use static() to load index, create about, help, index html

--- src/app.js
+++ src/app.js
@@ -1,25 +1,37 @@
+const path = require('path');
 const express = require('express');
+
 const app = express();
+
+const publicDirectoryPath = path.join(__dirname, '../public')
+app.use(express.static(publicDirectoryPath))
 
 app.listen(3000, () => {
     console.log('Server is up on port 3000');
 })
 
 
-app.get('', (req, res) => {
-    res.send('Hello express!')
-})
+// Dont need this root route anymore since express.static() is already serving it up
+// app.get('', (req, res) => {
+//     res.send('Hello express!')
+// })
 
-app.get('/help', (req, res) => {
-    res.send('Help page')
-})
+// app.get('/help', (req, res) => {
+//     res.send({
+//         name: 'Mike',
+//         age: 38
+//     })
+// })
 
-app.get('/about', (req, res) => {
-    res.send('About page')
-})
+// app.get('/about', (req, res) => {
+//     res.send('<h1>About</h1>')
+// })
 
 app.get('/weather', (req, res) => {
-    res.send('Weather page')
+    res.send({
+        forecast: 'Sunny with 0% of rain.',
+        location: 'Miami'
+    })
 })
 
 //localhost:3000
