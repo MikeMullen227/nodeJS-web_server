fd62eb9b82b9a4dfcdf47d9c7ee088ea9d386a97
Mike Mullen <mmullen227@gmail.com>
2019-04-07 15:47:51 -0400

    initial commit, setup express and a few routes

---
+++ src/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+})
+
+
+app.get('', (req, res) => {
+    res.send('Hello express!')
+})
+
+app.get('/help', (req, res) => {
+    res.send('Help page')
+})
+
+app.get('/about', (req, res) => {
+    res.send('About page')
+})
+
+app.get('/weather', (req, res) => {
+    res.send('Weather page')
+})
+
+//localhost:3000
+
+//routes
+// app.com
+// app.com/help
+// app.com/about
